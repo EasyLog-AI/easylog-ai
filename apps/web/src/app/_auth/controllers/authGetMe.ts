e8fda326205aeeb8f0cd02ff81ee545b00866897
RickVM <vanmelisrick@gmail.com>
2025-10-01 17:45:55 +0200

    feat: authGetMe

--- apps/web/src/app/_auth/controllers/authGetMe.ts
+++ apps/web/src/app/_auth/controllers/authGetMe.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod';
+
 import { protectedProcedure } from '@/lib/trpc/procedures';
 
 const authGetMe = protectedProcedure
@@ -9,6 +11,26 @@ const authGetMe = protectedProcedure
       summary: 'Get the currently authenticated user'
     }
   })
+  .output(
+    z.object({
+      id: z.string().uuid(),
+      name: z.string().nullable(),
+      email: z.string().email(),
+      emailVerified: z.boolean().nullable(),
+      image: z.string().nullable(),
+      createdAt: z.date(),
+      updatedAt: z.date(),
+      memories: z.array(
+        z.object({
+          id: z.string().uuid(),
+          userId: z.string().uuid(),
+          content: z.string(),
+          createdAt: z.date(),
+          updatedAt: z.date()
+        })
+      )
+    })
+  )
   .query(async ({ ctx }) => {
     return ctx.user;
   });
