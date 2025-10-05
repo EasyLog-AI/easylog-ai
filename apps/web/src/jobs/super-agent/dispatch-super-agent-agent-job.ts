70717944140ea6013218b35e6b10c46712cc1877
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-05 10:56:43 +0200

    feat: adds mcp

--- apps/web/src/jobs/super-agent/dispatch-super-agent-agent-job.ts
+++ apps/web/src/jobs/super-agent/dispatch-super-agent-agent-job.ts
@@ -1,12 +1,14 @@
 import { AbortTaskRunError, logger, schedules } from '@trigger.dev/sdk';
+import { eq } from 'drizzle-orm';
 
 import db from '@/database/client';
+import { superAgents } from '@/database/schema';
 
 import { runSuperAgentJob } from './run-super-agent-job';
 
 export const dispatchSuperAgentJob = schedules.task({
   id: 'dispatch-super-agents',
-  run: async ({ externalId }) => {
+  run: async ({ externalId, scheduleId }) => {
     if (!externalId) {
       throw new AbortTaskRunError('External ID not found');
     }
@@ -34,6 +36,13 @@ export const dispatchSuperAgentJob = schedules.task({
     if (!superAgent) {
       throw new AbortTaskRunError('Super agent not found');
     }
+
+    await db
+      .update(superAgents)
+      .set({
+        scheduleId: scheduleId
+      })
+      .where(eq(superAgents.id, superAgent.id));
 
     const lastChatPerUser = Object.values(
       superAgent.agent.chats.reduce(
