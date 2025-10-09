a1475c294c43d2e61a6c0120e3cb8ad4b04ae56f
RickVM <vanmelisrick@gmail.com>
2025-10-09 16:35:52 +0200

    refactor

--- apps/web/src/util/deep-partial.ts
+++ apps/web/src/util/deep-partial.ts
@@ -1,8 +1,9 @@
 /**
  * Create a type from an object with all keys and nested keys set to optional.
  *
- * This utility type recursively makes all properties optional, including nested objects.
- * It handles various types including primitives, maps, sets, arrays, and nested objects.
+ * This utility type recursively makes all properties optional, including nested
+ * objects. It handles various types including primitives, maps, sets, arrays,
+ * and nested objects.
  */
 export type DeepPartial<T> = T extends
   | string
@@ -14,14 +15,13 @@ export type DeepPartial<T> = T extends
   | symbol
   | Date
   | RegExp
-  | Function
   ? T
   : T extends Map<infer K, infer V>
-  ? Map<K, DeepPartial<V>>
-  : T extends Set<infer U>
-  ? Set<DeepPartial<U>>
-  : T extends Array<infer U>
-  ? Array<DeepPartial<U>>
-  : T extends object
-  ? { [K in keyof T]?: DeepPartial<T[K]> }
-  : T;
+    ? Map<K, DeepPartial<V>>
+    : T extends Set<infer U>
+      ? Set<DeepPartial<U>>
+      : T extends Array<infer U>
+        ? Array<DeepPartial<U>>
+        : T extends object
+          ? { [K in keyof T]?: DeepPartial<T[K]> }
+          : T;
