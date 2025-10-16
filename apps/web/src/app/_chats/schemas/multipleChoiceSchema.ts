a74174235788b7cdde85675f237f75741a783d3c
Jasper Verbeet <jasperverbeet@gmail.com>
2025-10-16 16:34:27 +0200

    fix: fixes deployments

--- apps/web/src/app/_chats/schemas/multipleChoiceSchema.ts
+++ apps/web/src/app/_chats/schemas/multipleChoiceSchema.ts
@@ -4,7 +4,8 @@ const multipleChoiceSchema = z.object({
   question: z.string(),
   options: z.array(z.string()),
   answer: z.string().nullable().optional(),
-  id: z.string()
+  id: z.string(),
+  value: z.string().nullable().optional()
 });
 
 export type MultipleChoiceSchema = z.infer<typeof multipleChoiceSchema>;
